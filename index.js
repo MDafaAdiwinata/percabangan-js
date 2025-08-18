c23e284266e14e761d44addd1fcd3e34fbb71ad0
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-08-18 11:12:10 +0700

    Cleaning and Fix Code JS

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 function validateNumber() {
-    event.preventDefault();
+  event.preventDefault();
 
-    let inputAngka = document.getElementById('inputAngka');
-    let outputText = document.getElementById('outputText');
-    let cardOutput = document.getElementById('cardOutput');
+  let inputAngka = document.getElementById("inputAngka").value;
+  let outputText = document.getElementById("outputText");
+  let cardOutput = document.getElementById("cardOutput");
 
-    cardOutput.classList.add("py-2");
+  cardOutput.classList.add("py-2");
 
-    if ( inputAngka == "" ) {
-        outputText.innerText = "Masukkan angka!";
-    } else if ( inputAngka % 2 == 0 ) {
-        outputText.innerText = "Genap";
-    } else if ( inputAngka % 2 == 1 ) {
-        outputText.innerText = "Ganjil";
-    } else {
-        outputText.innerText = "Yang anda masukkan bukanlah Angka!";
-    }
-
+  if (inputAngka == "") {
+    outputText.innerText = "Masukkan angka!";
+  } else if (inputAngka % 2 == 0) {
+    outputText.innerText = "Genap";
+  } else if (inputAngka % 2 == 1) {
+    outputText.innerText = "Ganjil";
+  } else {
+    outputText.innerText = "Yang anda masukkan bukanlah Angka!";
+  }
 }
