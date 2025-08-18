e08f15fbe22320ee76fd6c36bc57cbc1e8ce94b3
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-08-18 11:02:03 +0700

    Add Variable (Input, Output Text, Card)

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function validateNumber() {
     event.preventDefault();
+
+    let inputAngka = document.getElementById('inputAngka');
+    let outputText = document.getElementById('outputText');
+    let cardOutput = document.getElementById('cardOutput');
+
+    
 }
